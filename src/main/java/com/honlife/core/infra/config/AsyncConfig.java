abe6d50ebad5c4476ba197c54b60c799df3b9cda
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2026-03-02 14:41:30 +0900

    :recycle: refactor: 멀티스레드 환경의 자원 경합 해결을 위한 AsyncConfig

---
+++ src/main/java/com/honlife/core/infra/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.honlife.core.infra.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AsyncConfig {
+  @Bean(name = "homeApiExecutor")
+  @Primary
+  public Executor homeApiExecutor(){
+    ThreadPoolTaskExecutor es = new ThreadPoolTaskExecutor();
+
+    es.setCorePoolSize(5);
+    es.setMaxPoolSize(15);
+    es.setQueueCapacity(50);
+
+    es.initialize();
+    return es;
+  }
+
+}
